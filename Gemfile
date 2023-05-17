7fe118b8dec2c59efcd0db5475584f0efb756f61
Pepijn Van Eeckhoudt <pepijn@vaneeckhoudt.net>
2023-05-17 09:58:38 +0200

    Read input from CSV file and print output as CSV

--- Gemfile
+++
@@ -1,4 +0,0 @@
-source "https://rubygems.org"
-
-gem 'rest-client'
-gem 'mechanize', '~> 2.9', '>= 2.9.1'
--- mobiscore.rb
+++ mobiscore.rb
@@ -62,66 +62,68 @@ def get_statstical_unit(lat_wgs84, lon_wgs84)
   REXML::XPath.each(doc, xpath_expr, namespace).map { |e| e.text }
 end
 
-street = 'wijnbergenstraat'
-number = '97'
-city = 'Kessel-Lo'
+def get_mobi_score(street, number, city)
+  location_json = get_json(
+    "https://loc.geopunt.be/geolocation/location?q=#{URI.encode_www_form_component("#{street} #{number} #{city}")}",
+    {
+      'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.4 Safari/605.1.15',
+      'Accept' => '*/*',
+      'Host' => 'loc.geopunt.be',
+      'Sec-Fetch-Site' => 'cross-site',
+      'Sec-Fetch-Dest' => 'empty',
+      'Sec-Fetch-Mode' => 'cors',
+      'Referer' => 'https://mobiscore.omgeving.vlaanderen.be/',
+      'Accept-Encoding' => 'gzip, deflate, br',
+      'Accept-Language' => 'nl-NL,nl;q=0.9',
+      'Origin' => 'https://mobiscore.omgeving.vlaanderen.be'
+    })
 
-location_json = get_json(
-  "https://loc.geopunt.be/geolocation/location?q=#{URI.encode_www_form_component("#{street} #{number} #{city}")}",
-  {
-    'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.4 Safari/605.1.15',
-    'Accept' => '*/*',
-    'Host' => 'loc.geopunt.be',
-    'Sec-Fetch-Site' => 'cross-site',
-    'Sec-Fetch-Dest' => 'empty',
-    'Sec-Fetch-Mode' => 'cors',
-    'Referer' => 'https://mobiscore.omgeving.vlaanderen.be/',
-    'Accept-Encoding' => 'gzip, deflate, br',
-    'Accept-Language' => 'nl-NL,nl;q=0.9',
-    'Origin' => 'https://mobiscore.omgeving.vlaanderen.be'
-  })
+  location = location_json['LocationResult'][0]['Location']
+  lat_wgs84 = location['Lat_WGS84']
+  lon_wgs84 = location['Lon_WGS84']
+  x = location['X_Lambert72']
+  y = location['Y_Lambert72']
 
-location = location_json['LocationResult'][0]['Location']
-lat_wgs84 = location['Lat_WGS84']
-lon_wgs84 = location['Lon_WGS84']
-x = location['X_Lambert72']
-y = location['Y_Lambert72']
+  mobi_score_url = "https://mobiscore.omgeving.vlaanderen.be/ajax/get-score?lat=#{lat_wgs84}&lon=#{lon_wgs84}"
+  mobi_score_json = get_json(
+    mobi_score_url,
+    {
+      'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.4 Safari/605.1.15',
+      'Accept' => '*/*',
+      'Host' => 'mobiscore.omgeving.vlaanderen.be',
+      'Sec-Fetch-Site' => 'same-origin',
+      'Sec-Fetch-Dest' => 'empty',
+      'Sec-Fetch-Mode' => 'cors',
+      'Accept-Encoding' => 'gzip, deflate, br',
+      'Accept-Language' => 'nl-NL,nl;q=0.9',
+      'Referer' => 'https://mobiscore.omgeving.vlaanderen.be/'
+    }
+  )
+  mobi_score = mobi_score_json['score']['scores']
 
-mobiscore_url = "https://mobiscore.omgeving.vlaanderen.be/ajax/get-score?lat=#{lat_wgs84}&lon=#{lon_wgs84}"
-mobiscore_json = get_json(
-  mobiscore_url,
-  {
-    'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.4 Safari/605.1.15',
-    'Accept' => '*/*',
-    'Host' => 'mobiscore.omgeving.vlaanderen.be',
-    'Sec-Fetch-Site' => 'same-origin',
-    'Sec-Fetch-Dest' => 'empty',
-    'Sec-Fetch-Mode' => 'cors',
-    'Referer' => 'https://mobiscore.omgeving.vlaanderen.be/',
-    'Accept-Encoding' => 'gzip, deflate, br',
-    'Accept-Language' => 'nl-NL,nl;q=0.9',
-    'Referer' => 'https://mobiscore.omgeving.vlaanderen.be/'
+  statistical_units = get_statstical_unit(lat_wgs84, lon_wgs84)
+
+  return {
+    :lat => lat_wgs84,
+    :lon => lon_wgs84,
+    :x => x,
+    :y => y,
+    :mobi_score => {
+      :total => mobi_score['totaal'],
+      :health => mobi_score['gezondheid'],
+      :education => mobi_score['onderwijs'],
+      :culture => mobi_score['ontspanning_sport_cultuur'],
+      :public_transportation => mobi_score['ov'],
+      :services => mobi_score['winkels_en_diensten']
+    },
+    :statistical_units => statistical_units
   }
-)
-
-puts "Input"
-puts "  Street: #{street}"
-puts "  Number: #{number}"
-puts "  City: #{city}"
-puts
-
-puts "Location"
-puts "  lon: #{lon_wgs84}"
-puts "  lat: #{lat_wgs84}"
-puts "  x: #{x}"
-puts "  y: #{y}"
-puts
-
-puts "Mobiscore"
-mobiscore_json['score']['scores'].each do |key, value|
-  puts "  #{key}: #{value}"
 end
-puts
 
-puts "Statistical units"
-get_statstical_unit(lat_wgs84, lon_wgs84).each { |s| puts "  #{s}" }
+puts "straat,huisnummer,gemeente,lon,lat,mobi_totaal,mobi_gezondheid,mobi_onderwijs,mobi_ontspanning,mobi_ov,mobi_winkel,su"
+File.foreach(ARGV[0]) do |line|
+  street, number, city = line.split(',')
+  score = get_mobi_score(street, number, city)
+  mobi_score = score[:mobi_score]
+  puts "#{street},#{number},#{city},#{score[:lon]},#{score[:lat]},#{mobi_score[:total]},#{mobi_score[:health]},#{mobi_score[:education]},#{mobi_score[:culture]},#{mobi_score[:public_transportation]},#{mobi_score[:services]},#{score[:statistical_units].first}"
+end
